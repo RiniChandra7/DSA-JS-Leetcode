c3d6c35b4332b6ed8440bdfb4b145f74eb0f737a
RiniChandra7 <71338625+RiniChandra7@users.noreply.github.com>
2022-05-03 00:00:03 +0530

    https://leetcode.com/problems/set-matrix-zeroes/

---
+++ setMatrixZeroes.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var setZeroes = function(matrix) {
+    let col1 = 0;
+    for (let i = 0; i < matrix.length; i++) {
+        if (matrix[i][0] == 0) {
+            col1 = 1;
+        }
+        for (let j = 1; j < matrix[0].length; j++) {
+            if (matrix[i][j] == 0) {
+                matrix[i][0] = 0;
+                matrix[0][j] = 0;
+            }
+        }
+    }
+    
+    for (let i = 1; i < matrix.length; i++) {
+        for (let j = 1; j < matrix[0].length; j++) {
+            if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+    
+    if (matrix[0][0] == 0) {
+        for (let j = 0; j < matrix[0].length; j++) {
+            matrix[0][j] = 0;
+        }
+    }
+    
+    if (col1 == 1) {
+        for (let i = 0; i < matrix.length; i++) {
+            matrix[i][0] = 0;
+        }
+    }
+}; 
